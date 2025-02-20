29f90a7a9a3e0c047c44d5d72dd08ca9ef0901f6
Jake Morgenstern <jmorgenstern15@gmail.com>
2025-02-20 09:53:20 -0500

    Added comments

--- src/Interaction/InteractableNote.cpp
+++ src/Interaction/InteractableNote.cpp
@@ -4,12 +4,12 @@
 #include "InteractionTrigger.h"
 #include "Tool.h"
 #include "../GameSystems.h"
-#include "../Player/MouseLook.h"
 #include "based/app.h"
 #include "based/engine.h"
 #include "based/input/keyboard.h"
 #include "based/scene/entity.h"
 
+// Listeners for interactable hover events
 void InteractableNoteSystem::OnInteractionHoverEnter(Tool* tool)
 {
 	BASED_TRACE("Note hover entered!");
@@ -20,6 +20,7 @@ void InteractableNoteSystem::OnInteractionHoverExit(Tool* tool)
 	BASED_TRACE("Note hover exited!");
 }
 
+// Called when E is pressed in range of a note
 void InteractableNoteSystem::OnInteract(Tool* tool)
 {
 	using namespace based;
@@ -50,8 +51,11 @@ void InteractableNoteSystem::Update(float deltaTime)
 		auto& note = registry.get<InteractableNote>(e);
 		auto& trigger = registry.get<InteractionTrigger>(e);
 
+		// System callbacks don't know what Note is currently being looked at
+		// so we store a pointer to it here
 		mCurrentNote = &note;
 
+		// Should only trigger this once, the first time the object is hovered
 		if (!trigger.mHoverEntered)
 		{
 			trigger.mHoverEntered = true;
@@ -60,6 +64,7 @@ void InteractableNoteSystem::Update(float deltaTime)
 
 		if (input::Keyboard::KeyDown(BASED_INPUT_KEY_E))
 		{
+			// Show note if it's closed, otherwise hide it
 			if (!note.mIsOpen)
 			{
 				OnInteract(trigger.mTool);
@@ -77,12 +82,18 @@ void InteractableNoteSystem::Update(float deltaTime)
 			}
 		}
 
+		// Again, only called once when the item stops being hovered
 		if (trigger.mShouldExit)
 		{
 			OnInteractionHoverExit(trigger.mTool);
 
+			// Removing the trigger means this note will no longer be
+			// considered when evaluating notes
 			trigger.mTool->SetCurrentTrigger(nullptr);
 			registry.remove<InteractionTrigger>(e);
 		}
 	}
+
+	// Clear the current note since we are no longer looking at any notes
+	mCurrentNote = nullptr;
 }
