81308632fa762ae25f9027e472c812dd88b3c269
Jake Morgenstern <jmorgenstern15@gmail.com>
2025-08-31 08:55:33 -0400

    Updated input config to allow more human readable names for types and keys

--- src/KeyImageBindings.cpp
+++ src/KeyImageBindings.cpp
@@ -9,20 +9,20 @@ std::unordered_map<std::string, std::string> kbmMap = {
 
 std::unordered_map<std::string, std::string> xboxMap = {
 	{"", ""},
-	{"a", "XboxSeriesX_A"},
-	{"b", "XboxSeriesX_B"}
+	{"Gamepad_FaceButton_Bottom", "XboxSeriesX_A"},
+	{"Gamepad_FaceButton_Right", "XboxSeriesX_B"}
 };
 
 std::unordered_map<std::string, std::string> psMap = {
 	{"", ""},
-	{"a", "PS5_Cross"},
-	{"b", "PS5_Circle"},
+	{"Gamepad_FaceButton_Bottom", "PS5_Cross"},
+	{"Gamepad_FaceButton_Right", "PS5_Circle"},
 };
 
 std::unordered_map<std::string, std::string> switchMap = {
 	{"", ""},
-	{"a", "Switch_A"},
-	{"b", "Switch_B"},
+	{"Gamepad_FaceButton_Bottom", "Switch_A"},
+	{"Gamepad_FaceButton_Right", "Switch_B"},
 };
 
 std::unordered_map<std::string, std::unordered_map<std::string, std::string>> mKeyMaps = {
